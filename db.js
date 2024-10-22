6e6bee145d589f57525375f0cd1618d51d865aec
Muskan0423 <muskankewlani123@.com>
2024-10-22 09:58:26 +0530

    changes for backend

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // const mongoURI = "mongodb://0.0.0.0:27017/";
 // const mongoURI = "mongodb+srv://harsh:Harsh9945khosla@cluster0.osfevs6.mongodb.net/muskan";
 // const mongoURI = "mongodb+srv://muskankewlani123:muskan@cluster0.vhwewr7.mongodb.net/";
-const mongoURI ="mongodb+srv://muskankewlanicimet:Vgj0O4kXtR5y84wn@cluster0.ebuz8.mongodb.net/"
+const mongoURI ="mongodb+srv://muskankewlanicimet:Vgj0O4kXtR5y84wn@cluster0.ebuz8.mongodb.net/test"
 
 const ConnectToMongo = async () => {
     try {
